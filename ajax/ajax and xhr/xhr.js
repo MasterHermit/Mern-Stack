ae0ba8380c0f9650513dd12c124c2cd70a248975
MasterHermit <rajpruthwi5534.ps@gmail.com>
2022-07-10 20:47:54 +0530

    XMLHttpRequest

---
+++ ajax/ajax and xhr/xhr.js
@@ -0,0 +1,23 @@
+document.getElementById("btn").addEventListener("click", loadData);
+
+function loadData() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "data.txt");
+
+  //Optional for loading bar
+  xhr.onprogress = function () {
+    console.log("Loading...");
+  };
+
+  xhr.onload = function () {
+    if (this.status == 200) {
+      console.log(this.responseText);
+    }
+  };
+  xhr.onerror = function () {
+    console.log("Error loading data");
+  };
+
+  xhr.send();
+}
